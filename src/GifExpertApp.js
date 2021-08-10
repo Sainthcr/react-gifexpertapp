1c48324d2979509a9e2f975466f2a46babbdfd93
Sainth Castillo <SainthCastillo96@gmail.com>
2021-08-10 11:15:33 -0300

    firts commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"; 
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+ const GifExpertApp = () =>{
+
+
+  const [ categories,setCategories]=useState(['One Punch']);
+
+return(  
+<>
+
+ <h2> GifExpertApp </h2>
+<AddCategory setCategories={setCategories}/>
+
+<hr/>
+
+<ol>
+
+{
+
+categories.map(item  => (
+
+< GifGrid 
+    key={item}
+    category={ item}/>
+
+))
+
+}
+
+</ol>
+</>
+)
+}
+
+export default GifExpertApp
